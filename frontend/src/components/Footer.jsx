133a2fe4b892d0cf1fff647c0169edc709044b56
lipsanabharati <lipsanabharati28@gmail.com>
2026-03-01 18:13:47 +0545

    header and footer added

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,43 @@
+"use client";
+
+export default function Footer()
+{
+    return(
+        <div className="relative bg-[url('/footer-box.svg')] bg-cover bg-center w-full h-[265px]">
+            {/* Overlay text */}
+            <div className="absolute inset-0 flex flex-col items-center justify-center text-white font-body">
+                <div className="flex flex-row gap-60 mx-[10px] mt-[80px]">
+                     {/* Hours */}
+                    <div className="flex flex-col gap-3">
+                        <div className="font-heading font-bold text-center text-xl">Hours</div>
+                        <div className="flex flex-col">
+                            <p>Sun-Fri: 10am-5pm</p>
+                            <p>Sat: Closed</p>
+                        </div>
+                    </div>
+
+                    {/* Location */}
+                    <div className="flex flex-col gap-3">
+                        <div className="font-heading font-bold text-center text-xl">Location</div>
+                        <div className="flex flex-col">
+                            <p>Maharajgunj-03,Kathmandu</p>
+                        </div>
+                    </div>
+
+                     {/* Contact */}
+                    <div className="flex flex-col gap-3">
+                        <div className="font-heading font-bold text-center text-xl">Contact</div>
+                        <div className="flex flex-col w-[200px]">
+                            <p>9802512888, 9840298608,01-4373154</p>
+                            <p>localfarmnepal@gmail.com</p>
+                        </div>
+                    </div>
+
+
+                </div>
+                <img src="logo-white.svg" className="mr-[5%]" />
+            </div>
+
+        </div>
+    );
+};
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,31 @@
+"use client";
+
+import {motion} from "framer-motion";
+
+export default function Header()
+{
+    return(
+        <motion.div 
+        className="flex flex-row gap-[55%] my-[20px] mx-[80px] p-2"
+        initial={{ y:-100 }}
+        animate={{ y:0}}
+        transition={{ duration: 2 }}
+        >
+            {/*Logo*/}
+            <div>
+              <img src="logo.svg" className="h-[45px] w-[170px]"/>
+            </div>
+
+
+            {/*Items*/}
+            <div className="font-heading flex flex-row gap-10 items-center">
+                <div>Home</div>
+                <div>About</div>
+                <div>Blog</div>
+                <div>Contact</div>
+                <div>Shops</div>
+            </div>
+
+        </motion.div>
+    );
+};
